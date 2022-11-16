00854802499e99e34e8f8f83467c4887f76329f8
Christiano0407 <placebo0704@gmail.com>
2022-11-16 10:32:18 -0600

    consume API prove 01

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ const express = require(`express`);
 const app = express();
 const port = process.env.port || 3000;
 
+//const apiPlus = require(`../apiRest/js/apiApp`);
+
 app.get(`/`, (req, res) => {
-  res.send(`../apiRest/js/apiApp.js`);
+  res.send('Hello World');
 });
+
+//app.use(apiPlus);
 
 app.listen(port, () => {
   console.log(`Server is running in port http://localhost:${port}... `);
