d0f3ec258e33f4f5868ddcbe075dd39a69ed0748
Christiano0407 <placebo0704@gmail.com>
2022-11-16 12:33:44 -0600

    router

--- apiRest/js/apiApp.js
+++ apiRest/js/apiApp.js
@@ -1,3 +1,17 @@
 //** === === API REST === === */
-console.log('Hello');
 console.log('Consume API');
+
+const URL = `https://api.thecatapi.com/v1/images/search`;
+
+const call = async (api) => {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    const img = document.querySelector(`.img`);
+    img.src = data[0].url;
+  } catch (err) {
+    console.log('Have Error');
+  }
+};
+
+console.log(call(URL));
--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@ const express = require(`express`);
 const app = express();
 const port = process.env.port || 3000;
 
-//const apiPlus = require(`../apiRest/js/apiApp`);
+//const apiPlus = require(`../apiRest/js/apiApp.js`);
 
 app.get(`/`, (req, res) => {
   res.send('Hello World');
 });
 
-//app.use(apiPlus);
+app.get(`/api/v1/cat`, (req, res) => {
+  res.send(`Hello Cat`);
+});
+
+//app.use(`/cats`, apiPlus);
 
 app.listen(port, () => {
   console.log(`Server is running in port http://localhost:${port}... `);
