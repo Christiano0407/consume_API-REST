3e177b6afb0a687393f2e5c104fd9923570319cd
Christiano0407 <placebo0704@gmail.com>
2022-11-17 10:52:23 -0600

    add new picturres

--- apiRest/js/apiApp.js
+++ apiRest/js/apiApp.js
@@ -1,8 +1,10 @@
 //*TODO === === === API REST === === ==< */
 const btn = document.querySelector(`#btnId`);
+const btnCat = document.querySelector(`#btnIdCat`);
 
 //** === API >> */
 const API_URL = `https://api.thecatapi.com/v1/images/search`;
+const URL_CAT = `https://api.thecatapi.com/v1/images/search?limit=2`;
 
 const call = async (api) => {
   try {
@@ -22,3 +24,23 @@ const addCat = () => {
 btn.addEventListener('click', addCat);
 //*! Reload */
 //addCat();
+
+const callCat = async (apiUrl) => {
+  try {
+    //let addCat = [];
+    const res = await fetch(apiUrl);
+    const dataCat = await res.json();
+    //console.log(dataCat);
+    const imgCats = document.querySelectorAll(`#idImgCat`);
+    imgCats.forEach((catImg) => (catImg.src = dataCat[0].url));
+    //addCat.push(imgCats);
+  } catch (err) {
+    console.log('We have New Error!!');
+  }
+};
+
+const newCat = () => {
+  callCat(URL_CAT);
+};
+
+btnCat.addEventListener('click', newCat);
